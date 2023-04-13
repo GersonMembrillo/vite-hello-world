113dc1d50221b4c9e346caac2287c84ff78f1c40
GersonMembrillo <gersonmembrillo@hotmail.com>
2023-04-13 21:59:18 +0200

    remake

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <AppTitle />
+  </header>
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ titolo }}</h1>
+</template>
+
+<script >
+export default {
+  name: 'AppTitle',
+  data() {
+    return {
+      titolo: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
